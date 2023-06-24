62466c1ecb0071a02c44c41ef473721ac948826e
ch04937 <97hernandez.c@gmail.com>
2023-06-23 21:34:47 -0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-// import Header from "./components/Header";
-import { Header } from "nexious-library";
+import { Footer, Header } from "nexious-library";
 import data from "./data/example.data.json";
-// import { Header } from "nexious-library";
 
 function App({ children }) {
   return (
     <div className="app">
-      <Header
-        menu={data.menu}
-        data={{ url: data.logo.url, alt: data.logo.alt, title: data.title }}
-      />
+      <Header menu={data.menu} data={data.logo} title={data.title} />
       {children}
+      <Footer title={data.title} />
     </div>
   );
 }
